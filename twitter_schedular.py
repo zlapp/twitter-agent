75433bdf4ce41cee4307866a3f282927ad2bfcda
Simon <simonjudd2@gmail.com>
2023-04-26 17:41:09 -0600

    fix dumb error

--- twitter_schedular.py
+++ twitter_schedular.py
@@ -16,11 +16,11 @@ def weighted_random_choice(actions, probabilities):
 
 def perform_action():
     actions = [
-        params["reply_to_new_direct_messages"],
-        params["quote_tweet"],
-        params["post_tweet"],
-        params["like_timeline_tweets"],
-        params["respond_to_timeline_tweets"]
+        "reply_to_new_direct_messages",
+        "quote_tweet",
+        "post_tweet",
+        "like_timeline_tweets",
+        "respond_to_timeline_tweets",
     ]
 
     probabilities = [
