a56cd29321b7a211f235d10a74cd941ccf8c17a5
Simon <simonjudd2@gmail.com>
2023-05-04 10:22:02 -0600

    fix trend stream

--- twitter_trend_stream.py
+++ twitter_trend_stream.py
@@ -1,7 +1,7 @@
-import tweepy
-import matplotlib.pyplot as plt
-import datetime
 import os
+import tweepy
+import random
+import queue
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -20,30 +20,17 @@ auth = tweepy.OAuth1UserHandler(
 api = tweepy.API(auth)
 screen_name = "lil_bigsky_agi"
 
-user = api.get_user(screen_name=screen_name)
-followers_count = user.followers_count
-print(followers_count)
-created_at = user.created_at
+la_trend = api.get_place_trends(id=2442047)
+print('LOS ANGELES TRENDS')
+for i in range(10):
+    print(la_trend[0]["trends"][i]["name"])
 
-date_list = []
-followers_list = []
+sf_trend = api.get_place_trends(id=2487956)
+print('SF TRENDS')
+for i in range(40):
+    print(sf_trend[0]["trends"][i]["name"])
 
-for status in tweepy.Cursor(api.user_timeline, tweet_mode='extended').items():
-    date_list.append(status.created_at)
-    followers_list.append(status.user.followers_count)
-
-# Create a matplotlib graph
-plt.figure(figsize=(12, 6))
-plt.plot(date_list, followers_list)
-plt.xlabel('Date')
-plt.ylabel('Followers')
-plt.title(f'Twitter Followers Over Time for @{screen_name}')
-plt.grid()
-
-# Save the graph as a PNG file
-if not os.path.exists("graphs"):
-    os.mkdir("graphs")
-
-graph_filename = f"graphs/{screen_name}_followers_over_time.png"
-plt.savefig(graph_filename, dpi=300)
-plt.close()
+nyc_trend = api.get_place_trends(id=2459115)
+print('NYC TRENDS')
+for i in range(10):
+    print(nyc_trend[0]["trends"][i]["name"])
